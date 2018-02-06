a4756ae36692d04bd477ab125538dd570a04b68e
NarvinChana <36044306+NarvinChana@users.noreply.github.com>
2018-02-06 21:24:06 +0100

    Add files via upload

--- ProjetISN/play.js
+++ ProjetISN/play.js
@@ -1,1 +1,19 @@
-console.log("hi");
+var Application = PIXI.Application,
+    Sprite = PIXI.Sprite,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources;
+
+var app = new Application({
+    width: 1024,
+    height: 720,
+    antialias: true
+});
+
+document.body.appendChild(app.view);
+
+loader.add("lib/cat.png").load(draw);
+
+function draw() {
+    let cat = new Sprite(resources["lib/cat.png"].texture);
+    app.stage.addChild(cat);
+}
