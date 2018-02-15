c3c9ed5de2ff599a637a4a45623a29e6930c9d92
NarvinChana <36044306+NarvinChana@users.noreply.github.com>
2018-02-15 17:00:41 +0100

    Add files via upload

--- ProjetISN/play.js
+++ ProjetISN/play.js
@@ -10,11 +10,27 @@ var app = new Application({
 });
 document.getElementById("jeu").appendChild(app.view);
 
-loader.add("lib/tilesets/09.png").load(setup);
-
+loader
+.add("lib/tilesets/09.png")
+.load(setup);
+var cat;
 function setup(){
-	let texture = TextureCache["images/tilesets/09.png"];
+	/*let texture = PIXI.utils.TextureCache["images/tilesets/09.png"];
 	
-	let rectangle = new PIXI.Rectangle(192,195,91,19);
+	let rectangle = new PIXI.Rectangle(0, 0, 32, 32);
+	
+	PIXI.texture.frame = rectangle;*/
+	
+	cat = new Sprite(resources["lib/tilesets/09.png"].texture);
+	
+	cat.x = 500;
+	cat.y = 100;
+	
+	app.stage.addChild(cat);
+	
+	app.ticker.add(delta => gameLoop(delta));
 }
 
+function gameLoop(delta){
+	
+}
