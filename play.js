7d06858aae5f4f98590f202562386a3ad844e45d
NarvinChana <36044306+NarvinChana@users.noreply.github.com>
2018-02-26 22:01:43 +0100

    Intégration de la hover sur les boutons.
    Ameillorations code menu principale

--- play.js
+++ play.js
@@ -1,7 +1,6 @@
 var gameWidth = window.innerWidth,
     gameHeight = window.innerHeight;
 
-
 //Des alias qui permettent de raccourcir le code
 var Application = PIXI.Application,
     Container = PIXI.Container,
@@ -19,6 +18,7 @@ var app = new Application({
     height: gameHeight,
     antialias: true
 });
+
 document.getElementById("jeu").appendChild(app.view);
 
 //Chargement des images
@@ -38,19 +38,17 @@ loader
 //variables qui seront utilisé dans plusieurs fonctions
 var state;
 var menuScene, gameScene;
-var play, play_button, play_hover, options, quit;
-var buttons = [],
-    buttons_texture = [];
-app.renderer.autoResize = true;
+var buttons_texture = [];
+var play, options, quit;
+var button_container;
 
+app.renderer.autoResize = true;
 //Initialisation des images, textures et containers
 function setup() {
-    
     //fond
     var bg = new Sprite(resources["lib/main menu/background.png"].texture);
     bg.width = gameWidth;
     bg.height = gameHeight;
-    
     //titre
     var title = new Sprite(resources["lib/main menu/title.png"].texture);
     title.width = gameWidth * 80 / 100;
@@ -58,67 +56,47 @@ function setup() {
     title.x = gameWidth / 2 - title.width / 2;
     title.y = 7 / 100 * gameHeight;
     
-    //mise en place de l'écran menu (titre + fond) dans la scène
-    app.stage.addChild(bg, title);
-    
-    buttons_texture[]{
+    buttons_texture = [
         TextureCache["lib/main menu/buttons/play.png"],
         TextureCache["lib/main menu/buttons/play_hover.png"],
         TextureCache["lib/main menu/buttons/options.png"],
         TextureCache["lib/main menu/buttons/options_hover.png"],
         TextureCache["lib/main menu/buttons/quit.png"],
-        TextureCache["lib/main menu/buttons/quit_hover.png"],
-    }
+        TextureCache["lib/main menu/buttons/quit_hover.png"]
+    ]
     
-    for (var i = 0; i < 3; i++){
-        buttons[i] = new Sprite();
-        buttons[i].texture = buttons_texture[i*2];
-        buttons[i].width = gameWidth * 40 / 100;
-        buttons[i].height = gameHeight * 15 / 100;
-        buttons[i].x = 
-        buttons[i].y =
-        buttons[i].interactive = true;
-        buttons[i].on('mouseover', function(){this.texture = buttons_texture[i*2];});
-        buttons[i].on('mouseout', function(){this.texture = buttons_texture[i*2+1];});
-        
-    }
-    //bouton Jouer
+    button_container = new Container();
+    
     play = new Sprite();
-    play_button = TextureCache["lib/main menu/buttons/play.png"];
-    play_hover = TextureCache["lib/main menu/buttons/play_hover.png"];
-    play.texture = play_button;
-    play.width = gameWidth * 40 / 100;
-    play.height = gameHeight * 15 / 100;
-    play.x = gameWidth / 2 - play.width / 2;
-    play.y = gameHeight * 45 / 100;
-    play.interactive = true;
-    play.on('mouseover', function () {this.texture = play_hover;});
-    play.on('mouseout', function () {this.texture = play_button;});
-
-    //bouton Options 
-    options = new Sprite(resources["lib/main menu/buttons/options.png"].texture);
-    play_hover = TextureCache["lib/main menu/buttons/play_hover.png"];
-    options.width = gameWidth * 40 / 100;
-    options.height = gameHeight * 15 / 100;
-    options.x = gameWidth / 2 - options.width / 2;
-    options.y = gameHeight * 45 / 100 + options.height;
-    options.interactive = true;
-    options.on('mouseover', function() {this.texture = options_hover;});
-    options.on('mouseout', function() {this.texture = options_button;});
+    options = new Sprite();
+    quit = new Sprite;
     
-    //bouton Quitter
-    quit = new Sprite(resources["lib/main menu/buttons/quit.png"].texture);
-    quit.width = gameWidth * 40 / 100;
-    quit.height = gameHeight * 15 / 100;
-    quit.x = gameWidth / 2 - quit.width / 2;
-    quit.y = gameHeight * 45 / 100 + quit.height * 2;
-    quit.interactive = true;
-    quit.on('mouseover', function() {this.texture = quit_hover;});
-    quit.on('mouseout', function() {this.texture = quit_button;});
-    //menuScene = new Container();
-    //menuScene.addChild(play, options, quit);
-    app.stage.addChild(play, options, quit);
-
+    button_container.addChild(play, options, quit);
+    
+    for(var i = 0; i < 3; i++) {
+        button_container.getChildAt(i).texture = buttons_texture[i*2];
+        button_container.getChildAt(i).width = gameWidth * 40 / 100;
+        button_container.getChildAt(i).height = gameHeight * 15 / 100;
+        button_container.getChildAt(i).x = gameWidth / 2 - button_container.getChildAt(i).width / 2;
+        button_container.getChildAt(i).y = gameHeight * 45/100 + button_container.getChildAt(i).height * i; 
+        button_container.getChildAt(i).interactive = true;
+    }
+    
+    play.on('mouseover', function(){play.texture = buttons_texture[1];});
+    play.on('mouseout', function(){play.texture = buttons_texture[0];});
+    
+    options.on('mouseover', function(){options.texture = buttons_texture[3];});
+    options.on('mouseout', function(){options.texture = buttons_texture[2];});
+    
+    quit.on('mouseover', function(){quit.texture = buttons_texture[5];});
+    quit.on('mouseout', function(){quit.texture = buttons_texture[4];});
+    //mise en place de l'écran menu (titre + fond) dans la scène
+    app.stage.addChild(bg, title);
+    
+    menuScene = new Container();
+    menuScene.addChild(button_container);
+    app.stage.addChild(menuScene);
+    
     gameScene = new Container();
     app.stage.addChild(gameScene);
 
@@ -129,18 +107,25 @@ function setup() {
 
 //Boucle général du jeu
 function gameLoop() {
-    //app.renderer.resize(window.innerWidth,window.innerHeight);
+
     state();
-    window.requestAnimationFrame(gameLoop);
+    //window.requestAnimationFrame(gameLoop);
 }
 
 //Tout le code du menu principal est placé ici
 function menu() {
-
+    play.on("click", function() {
+        
+    })
+    options.on("click", function(){
+        
+    })
+    quit.on("click", function(){
+        
+    })
 
 }
 //Tout le code du jeu est placé ici
 function play() {
 
 }
-
