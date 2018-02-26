8a8cf340f3276d3b20e1c141854910a2547f7bf6
Leny Marcolini <36044297+LenyMarco@users.noreply.github.com>
2018-02-26 19:04:43 +0100

    Add files via upload

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 var gameWidth = window.innerWidth,
-    gameHeight= window.innerHeight;
+    gameHeight = window.innerHeight;
+
 
 //Des alias qui permettent de raccourcir le code
 var Application = PIXI.Application,
@@ -38,54 +39,63 @@ loader
 var state;
 var menuScene, gameScene;
 var play, play_button, play_hover, options, quit;
-app.renderer.autoResize= true;
+app.renderer.autoResize = true;
 //Initialisation des images, textures et containers
 function setup() {
-    
+    //fond
     var bg = new Sprite(resources["lib/main menu/background.png"].texture);
     bg.width = gameWidth;
     bg.height = gameHeight;
-
+    //titre
     var title = new Sprite(resources["lib/main menu/title.png"].texture);
-    title.width = gameWidth*80/100;
-    title.height = gameHeight*20/100;
-    title.x = gameWidth/2-title.width/2;
-    title.y = 7/100*gameHeight;
+    title.width = gameWidth * 80 / 100;
+    title.height = gameHeight * 20 / 100;
+    title.x = gameWidth / 2 - title.width / 2;
+    title.y = 7 / 100 * gameHeight;
     
-    app.stage.addChild(bg,title);
+    //mise en place de l'écran menu (titre + fond) dans la scène
+    app.stage.addChild(bg, title);
     
+    //bouton Jouer
     play = new Sprite();
     play_button = TextureCache["lib/main menu/buttons/play.png"];
     play_hover = TextureCache["lib/main menu/buttons/play_hover.png"];
     play.texture = play_button;
-    play.width = gameWidth*40/100;
-    play.height = gameHeight*10/100;
-    play.x = gameWidth/2-play.width/2;
-    play.y = gameHeight*45/100;
-    play.setInteractive = true;
-    play.on('mouseover', onMouseOver);
-    play.on('mouseout', onMouseOut);
-    
-    
+    play.width = gameWidth * 40 / 100;
+    play.height = gameHeight * 15 / 100;
+    play.x = gameWidth / 2 - play.width / 2;
+    play.y = gameHeight * 45 / 100;
+    play.interactive = true;
+    play.on('mouseover', function () {this.texture = play_hover;});
+    play.on('mouseout', function () {this.texture = play_button;});
+
+    //bouton Options 
     options = new Sprite(resources["lib/main menu/buttons/options.png"].texture);
-    options.width = gameWidth*40/100;
-    options.height = gameHeight*10/100;
-    options.x = gameWidth/2-options.width/2;
-    options.y = gameHeight*45/100+options.height*1.5;
+    play_hover = TextureCache["lib/main menu/buttons/play_hover.png"];
+    options.width = gameWidth * 40 / 100;
+    options.height = gameHeight * 15 / 100;
+    options.x = gameWidth / 2 - options.width / 2;
+    options.y = gameHeight * 45 / 100 + options.height;
+    options.interactive = true;
+    options.on('mouseover', function() {this.texture = options_hover;});
+    options.on('mouseout', function() {this.texture = options_button;});
     
+    //bouton Quitter
     quit = new Sprite(resources["lib/main menu/buttons/quit.png"].texture);
-    quit.width = gameWidth*40/100;
-    quit.height = gameHeight*10/100;
-    quit.x = gameWidth/2-quit.width/2;
-    quit.y = gameHeight*45/100+quit.height*1.5*2;
-    
+    quit.width = gameWidth * 40 / 100;
+    quit.height = gameHeight * 15 / 100;
+    quit.x = gameWidth / 2 - quit.width / 2;
+    quit.y = gameHeight * 45 / 100 + quit.height * 2;
+    quit.interactive = true;
+    quit.on('mouseover', function() {this.texture = quit_hover;});
+    quit.on('mouseout', function() {this.texture = quit_button;});
     //menuScene = new Container();
     //menuScene.addChild(play, options, quit);
-    app.stage.addChild(play,options,quit);
-    
+    app.stage.addChild(play, options, quit);
+
     gameScene = new Container();
     app.stage.addChild(gameScene);
-    
+
     state = menu;
 
     gameLoop();
@@ -100,19 +110,16 @@ function gameLoop() {
 
 //Tout le code du menu principal est placé ici
 function menu() {
-    
-    
-    
+
+
 }
 //Tout le code du jeu est placé ici
 function play() {
-    
+
 }
 
-function onMouseOver() {
-    this.texture = play_hover;
-}
 
 function onMouseOut() {
+
     this.texture = play_button;
 }
