869b4f842c4f80537c3442fb096274f17cedd602
NarvinChana <36044306+NarvinChana@users.noreply.github.com>
2018-02-27 17:10:22 +0100

    Ajout de l'intro et création de profile + souris custom

--- play.js
+++ play.js
@@ -38,11 +38,11 @@ loader
 //variables qui seront utilisé dans plusieurs fonctions
 var state;
 var introScene, menuScene, gameScene;
-var buttons_texture = [];
-var play, options, quit;
 var button_container;
-var welcome, text, inputField, inputText, input;
+var welcome, text;
 var bg, title;
+var play, options, quit;
+var playerName = undefined;
 app.renderer.autoResize = true;
 //Initialisation des images, textures et containers
 function setup() {
@@ -50,43 +50,31 @@ function setup() {
     bg = new Sprite(resources["lib/main menu/background.png"].texture);
     bg.width = gameWidth;
     bg.height = gameHeight;
-    /*
+
     //intro
     welcome = new Sprite(resources["lib/main menu/title.png"].texture);
     welcome.width = gameWidth * 80 / 100;
     welcome.height = gameHeight * 20 / 100;
     welcome.x = gameWidth / 2 - welcome.width / 2;
     welcome.y = 7 / 100 * gameHeight;
-    
-    text = new Text('Bienvenue à Space Attack!! Veuillez rentrer un nom d\'utilisateur' ,{fontFamily : 'Arial', fontSize: 24, fill : 'black', align : 'center'});
-    text.position.set(gameWidth/2-24*15,gameHeight/2);
-    
-    inputField = new Sprite(resources["lib/main menu/buttons/play.png"].texture);
-    inputField.interactive = true;
-    inputField.width = 300;
-    inputField.height = 300;
-    inputField.position.set(gameWidth/2,gameHeight/2);
-    
-    input = new Text(inputText,{fontFamily : 'Arial', fontSize: 24, fill : 'black', align : 'center'});
-    input.position.set(gameWidth/2,gameHeight/2);
-    
-    */
-    
-    
-    
-    
-    
-    
-    
-    
+
+    text = new Text('Bienvenue à Space Attack!! Veuillez rentrer un nom d\'utilisateur', {
+        fontFamily: 'Arial',
+        fontSize: 24,
+        fill: 'black',
+        align: 'center'
+    });
+    text.position.set(gameWidth / 2 - 24 * 15, gameHeight / 2);
+
+
     //titre
     title = new Sprite(resources["lib/main menu/title.png"].texture);
     title.width = gameWidth * 80 / 100;
     title.height = gameHeight * 20 / 100;
     title.x = gameWidth / 2 - title.width / 2;
     title.y = 7 / 100 * gameHeight;
-    
-    buttons_texture = [
+
+    var buttons_texture = [
         TextureCache["lib/main menu/buttons/play.png"],
         TextureCache["lib/main menu/buttons/play_hover.png"],
         TextureCache["lib/main menu/buttons/options.png"],
@@ -94,46 +82,57 @@ function setup() {
         TextureCache["lib/main menu/buttons/quit.png"],
         TextureCache["lib/main menu/buttons/quit_hover.png"]
     ]
-    
+
     button_container = new Container();
-    
+
     play = new Sprite();
     options = new Sprite();
     quit = new Sprite;
-    
+
     button_container.addChild(play, options, quit);
-    
-    for(var i = 0; i < 3; i++) {
-        button_container.getChildAt(i).texture = buttons_texture[i*2];
+
+    for (var i = 0; i < 3; i++) {
+        button_container.getChildAt(i).texture = buttons_texture[i * 2];
         button_container.getChildAt(i).width = gameWidth * 40 / 100;
         button_container.getChildAt(i).height = gameHeight * 15 / 100;
         button_container.getChildAt(i).x = gameWidth / 2 - button_container.getChildAt(i).width / 2;
-        button_container.getChildAt(i).y = gameHeight * 45/100 + button_container.getChildAt(i).height * i; 
+        button_container.getChildAt(i).y = gameHeight * 45 / 100 + button_container.getChildAt(i).height * i;
         button_container.getChildAt(i).interactive = true;
-        button_container.getChildAt(i).buttonMode = true;
     }
-    
-    play.on('mouseover', function(){play.texture = buttons_texture[1];});
-    play.on('mouseout', function(){play.texture = buttons_texture[0];});
-    
-    options.on('mouseover', function(){options.texture = buttons_texture[3];});
-    options.on('mouseout', function(){options.texture = buttons_texture[2];});
-    
-    quit.on('mouseover', function(){quit.texture = buttons_texture[5];});
-    quit.on('mouseout', function(){quit.texture = buttons_texture[4];});
+
+    play.on('mouseover', function () {
+        play.texture = buttons_texture[1];
+    });
+    play.on('mouseout', function () {
+        play.texture = buttons_texture[0];
+    });
+
+    options.on('mouseover', function () {
+        options.texture = buttons_texture[3];
+    });
+    options.on('mouseout', function () {
+        options.texture = buttons_texture[2];
+    });
+
+    quit.on('mouseover', function () {
+        quit.texture = buttons_texture[5];
+    });
+    quit.on('mouseout', function () {
+        quit.texture = buttons_texture[4];
+    });
     //mise en place de l'écran menu (titre + fond) dans la scène
-    
+
     app.stage.addChild(bg);
-    
-    /*introScene = new Container();
-    introScene.addChild(welcome,text);
-    app.stage.addChild(inputField,input);
+
+    introScene = new Container();
+    introScene.addChild(welcome, text);
     app.stage.addChild(introScene);
-    */
+
     menuScene = new Container();
-    menuScene.addChild(title,button_container);
+    menuScene.addChild(title, button_container);
     app.stage.addChild(menuScene);
-    
+    menuScene.visible = false;
+
     gameScene = new Container();
     app.stage.addChild(gameScene);
 
@@ -144,43 +143,73 @@ function setup() {
 
 //Boucle général du jeu
 function gameLoop() {
-
     state();
-    //window.requestAnimationFrame(gameLoop);
+    window.requestAnimationFrame(gameLoop);
 }
 
 //Tout le code du menu principal est placé ici
 function menu() {
-    /*
-    button_container.visible = false;
-    title.visible = false;
-    
-    var tempText;
-    var x = 0;
-    window.addEventListener("keypress",function(e){
-        tempText = e.keyCode;
-        x+=1;
-        if(tempText == 8){
-            x -= 2;    
-        }
-    });
-    inputText.replace(inputText.substring(x,x+1),String.fromCharCode(tempText));
-    console.log(inputText);
-    */
-    
-    play.on("click", function() {
+
+    if (playerName != undefined) {
+        introScene.visible = false;
+        menuScene.visible = true;
+    }
+
+    play.on("click", function () {
         menuScene.visible = false;
         state = play;
     })
-    options.on("click", function(){
+    options.on("click", function () {
         button_container.visible = false;
     })
-    quit.on("click", function(){
-        
-    })
+    quit.on("click", function () {
 
+    })
 }
 //Tout le code du jeu est placé ici
 function play() {
     //insérer code du jeu
+}
+
+function keyboard(keyCode) {
+    var key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    //The `downHandler`
+    key.downHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+        }
+        event.preventDefault();
+    };
+
+    //The `upHandler`
+    key.upHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+        }
+        event.preventDefault();
+    };
+
+    //Attach event listeners
+    window.addEventListener(
+        "keydown", key.downHandler.bind(key), false
+    );
+    window.addEventListener(
+        "keyup", key.upHandler.bind(key), false
+    );
+    return key;
+}
+
+function profile() {
+    playerName = document.getElementById("userInput").value;
+    x = document.getElementById("input");
+    x.style.display = 'none';
 }
